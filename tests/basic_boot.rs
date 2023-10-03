9bd70b27a91677eb7cfa2fa78148beb994498103
jquinn27 <jq01@outlook.com>
2023-10-03 16:47:19 -0400

    added testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,28 +5,21 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use quinn_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     quinn_os::test_panic_handler(info)
+}
+use quinn_os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
